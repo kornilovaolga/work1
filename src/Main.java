4ca3281f3c6acc4b93aa975b4748aa1df75b7a93
Olga <kornilovaolga1488@gmail.com>
2023-06-19 20:31:04 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int price = 12_200;
+        int miles = 20*1;
+        int bonusMiles = (price / miles);
+        System.out.println (bonusMiles);
+    }
+}
